fd69e99af6d10e3a7814f62c0a4dec9020b47487
trueazp <54744527+trueazp@users.noreply.github.com>
2020-04-13 13:08:34 +0800

    Update traditional way

--- Basic/Mode.py
+++ Basic/Mode.py
@@ -10,12 +10,22 @@ def mode_shortcut(data):
 
 # define function (traditional)
 def mode(data):
-    max_count = (0, 0)
-    for i in data:
-        occurences = data.count(i)
-        if occurences > max_count[0]:
-            max_count = (occurences, i)
-    return max_count[1]
+    element = 0
+    count = 0
+    for i in data[0:]:
+        temp_element = i
+        temp_count = 0
+        for j in data[0:]:
+            if j == temp_element:
+                temp_count += 1
+        if (temp_count > count):
+            element = temp_element
+            count = temp_count
+    if count == 1:
+        return data
+    else:
+        return element
+
 
 # declare a list
 data = []
