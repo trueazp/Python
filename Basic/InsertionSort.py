686497d4b690d65b8c0491468393b2f761967e46
miaowmerE <54744527+trueazp@users.noreply.github.com>
2020-09-17 07:14:27 +0800

    update

--- Basic/InsertionSort.py
+++ Basic/InsertionSort.py
@@ -3,19 +3,20 @@
 
 # define function
 def insertion_sort(list):
-  for i in range(1, len(list)):
-    key = list[i]
-    j = i - 1
-    while j >= 0 and key < list[j]:
-      list[j + 1] = list[j]
-      j -= 1
-    list[j + 1] = key
+    for i in range(1, len(list)):
+        key = list[i]
+        j = i - 1
+        while j >= 0 and key < list[j]:
+            list[j + 1] = list[j]
+            j -= 1
+        list[j + 1] = key
+
 
 # driver code
 if __name__ == "__main__":
-  list = []
-  for i in range(int(input("Length ::> "))):
-    list.append(int(input()))
-  insertion_sort(list)
+    list = []
+    for i in range(int(input("Length ::> "))):
+        list.append(int(input()))
+    insertion_sort(list)
 
-  print(list)
+    print(list)
